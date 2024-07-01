9b36a574281f1ed9c9669d51916eb515e58dfa58
Edmond <edpodlegaev@yandex.ru>
2024-07-01 20:34:04 +0300

    Routing. Code-Splitting. Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
+import {Route, Routes, Link} from 'react-router-dom';
 import "./index.scss";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            Hello, World!
-            <Counter />
+            <Link to={"/"}>Главная</Link>
+            <Link to={"/about"}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={"/"} element={<MainPageAsync />} />
+                    <Route path={"/about"} element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
