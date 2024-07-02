a93e1ffe3f77b530fd9cc69c586f5501563e282c
Edmond <edpodlegaev@yandex.ru>
2024-07-02 15:54:15 +0300

    Add Navbar. Templates for Development. First UI Kit Component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,14 @@ import "./styles/index.scss";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames("app", {}, [theme])}>
             <button onClick={toggleTheme}>Toggle Theme</button>
-            <Link to={"/"}>Главная</Link>
-            <Link to={"/about"}>О сайте</Link>
+            <Navbar />
             <AppRouter />
         </div>
     );
