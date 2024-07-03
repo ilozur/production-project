10b1c0d8fc8a6fe5085e602e516a078ae48a1ddc
Edmond <edpodlegaev@yandex.ru>
2024-07-03 18:43:02 +0300

    Add Babel, Extract Plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,6 +9,26 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -42,9 +62,10 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     }
 
     return [
-        typeScriptLoader,
-        cssLoader,
+        fileLoader,
         svgLoader,
-        fileLoader
+        babelLoader,
+        typeScriptLoader,
+        cssLoader
     ];
 }
