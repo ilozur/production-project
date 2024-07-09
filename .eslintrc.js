cdf124c8dd93eaef97350fbd3cf8d6daec34fe33
Edmond <edpodlegaev@yandex.ru>
2024-07-10 00:49:32 +0300

    Add Storybook & Decorators

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'max-len': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid'] }],
+        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }],
     },
     globals: {
         __IS_DEV__: true,
