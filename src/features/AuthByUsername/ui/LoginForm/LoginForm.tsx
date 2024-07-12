b68d8d25da02c100b1bdd52400aeff90a5609428
Edmond <edpodlegaev@yandex.ru>
2024-07-12 17:54:41 +0300

    fix

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -37,8 +37,21 @@ export const LoginForm = memo(({ className }: LoginFormProps) => {
         <div className={classNames(cls.LoginForm, {}, [className])}>
             <Text title={t('Форма авторизации')} />
             {error && <Text text={t('Вы ввели неверный логин или пароль')} theme={TextTheme.ERROR} />}
-            <Input autofocus type="text" className={cls.input} placeholder={t('Введите username')} onChange={onChangeUsername} value={username} />
-            <Input type="text" className={cls.input} placeholder={t('Введите пароль')} onChange={onChangePassword} value={password} />
+            <Input
+                autofocus
+                type="text"
+                className={cls.input}
+                placeholder={t('Введите username')}
+                onChange={onChangeUsername}
+                value={username}
+            />
+            <Input
+                type="text"
+                className={cls.input}
+                placeholder={t('Введите пароль')}
+                onChange={onChangePassword}
+                value={password}
+            />
             <Button theme={ButtonTheme.OUTLINE} className={cls.loginBtn} onClick={onLoginClick} disabled={isLoading}>
                 {t('Войти')}
             </Button>
