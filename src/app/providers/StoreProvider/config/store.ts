8427b9d63cb6ff7e5ba4301fac5de024d8a30d72
Edmond <edpodlegaev@yandex.ru>
2024-07-13 22:16:56 +0300

    Add Instance API

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -2,12 +2,15 @@ import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { useDispatch } from 'react-redux';
+import { NavigateOptions, To } from 'react-router-dom';
 import { StateSchema } from './StateSchema';
 import { createReducerManager } from './reducerManager';
+import { $api } from '../../../../shared/api/api';
 
 export function createReduxStore(
     initialState?: StateSchema,
     asyncReducers?: ReducersMapObject<StateSchema>,
+    navigate?: (to: To, options?: NavigateOptions) => void,
 ) {
     const rootReducers: ReducersMapObject<StateSchema> = {
         ...asyncReducers,
@@ -17,10 +20,18 @@ export function createReduxStore(
 
     const reducerManager = createReducerManager(rootReducers);
 
-    const store = configureStore<StateSchema>({
+    const store = configureStore({
         reducer: reducerManager.reduce,
         devTools: __IS_DEV__,
         preloadedState: initialState,
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+            thunk: {
+                extraArgument: {
+                    api: $api,
+                    navigate,
+                },
+            },
+        }),
     });
 
     // @ts-ignore
