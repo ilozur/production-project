46f9ef2e3f028b1948b6f5a2615839d6a883b4a6
Edmond <edpodlegaev@yandex.ru>
2024-08-05 17:28:50 +0300

    Update custom ESLint plugin, add support for aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'ilozur-plugin/path-checker': 'error',
+        'ilozur-plugin/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -18,7 +18,9 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             publicPath: '/',
         },
         plugins: buildPlugins(options),
-        module: { rules: buildLoaders(options) },
+        module: {
+            rules: buildLoaders(options),
+        },
         resolve: buildResolvers(options),
         devtool: isDev ? 'inline-source-map' : undefined,
         devServer: isDev ? buildDevServer(options) : undefined,
