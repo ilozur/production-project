3ebd5948c6589332e16597f6fa391ee235cc4f7a
Edmond <edpodlegaev@yandex.ru>
2024-08-15 18:37:09 +0300

    Add text for MainPage and AboutPage

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { Text } from '@/shared/ui/redesigned/Text';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { VStack } from '@/shared/ui/redesigned/Stack';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
 
-    return <Page data-testid="AboutPage">{t('О сайте')}</Page>;
+    return (
+        <Page data-testid="AboutPage">
+            <VStack gap="32" max>
+                <Card>
+                    {/* eslint-disable-next-line max-len */}
+                    <Text title={t('О сайте')} text={t('Этот сайт является частью моего резюме и служит для того, чтобы показать, на что я способен как профессионал. Моя цель — продемонстрировать не только свои технические навыки, но и навыки промышленного программирования и умение работать с современными frontend-технологиями')} />
+                </Card>
+            </VStack>
+        </Page>
+    );
 };
 
 export default AboutPage;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
+import { Card } from '@/shared/ui/redesigned/Card';
+import { Text } from '@/shared/ui/redesigned/Text';
+import { VStack } from '@/shared/ui/redesigned/Stack';
 
 const MainPage = () => {
-    const { t } = useTranslation();
-    const [value, setValue] = useState('');
-
-    const onChange = (val: string) => {
-        setValue(val);
-    };
+    const { t } = useTranslation('main');
 
     return (
         <Page data-testid="MainPage">
-            {t('Главная страница')}
+            <VStack gap="32" max>
+                <Card>
+                    {/* eslint-disable-next-line max-len */}
+                    <Text title={t('ilozur habr')} text={t('Добро пожаловать на ilozur habr! Этот проект был создан с целью продемонстрировать мои навыки и возможности в веб-разработке')} />
+                </Card>
+            </VStack>
         </Page>
     );
 };
